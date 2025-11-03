6ac98b9cc545132b1fa0e31f8f27c1859996dba5
Miguel <mrkaeding@gmail.com>
2025-11-03 11:55:23 +0100

    Update `ssl_cert_path` configuration for RedProvider and `.env.example` file

--- config/redprovider.php
+++ config/redprovider.php
@@ -6,7 +6,7 @@ return [
     'client_id' => env('REDPROVIDER_CLIENT_ID', 'Fun'),
     'client_secret' => env('REDPROVIDER_CLIENT_SECRET', '=work@red'),
 
-    'ssl_cert_path' => env('REDPROVIDER_SSL_CERT_PATH', base_path('RedProviderPortal/ssl_cert.pem')),
+    'ssl_cert_path' => env('REDPROVIDER_SSL_CERT_PATH', base_path('ssl_cert.pem')),
 
     'use_mock' => env('REDPROVIDER_USE_MOCK', true),
 ];
